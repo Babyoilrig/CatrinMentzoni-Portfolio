2c292abb736cef3e05153ab17ee990e5b843b5fa
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-02-28 16:19:33 +0000

    Initial Commit

---
+++ Header/index.js
@@ -0,0 +1,21 @@
+import css from "./Header.module.css";
+import Link from "next/link";
+
+
+export default function Navbar() {
+  return (
+    
+    <nav>
+    <div className={css.navSection}>
+      <img src="/images/mary-quant-flower-removebg-preview.png" width="60px"/>
+      <div className={css.navLinks}>
+        <Link href="/about"><span>About</span></Link>
+        <Link href="#projects-section"><span>Projects</span></Link>
+        {/* <Link href="#contact-section"><span>Contact</span></Link> */}
+      </div>
+      </div>
+    </nav>
+    
+    
+  );
+}
