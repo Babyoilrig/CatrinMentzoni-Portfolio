52e299ba0fc3b9bae9defe732702ef95b69a038a
Babyoilrig <93347177+Babyoilrig@users.noreply.github.com>
2022-03-06 15:21:23 +0000

    About me paragraph move to original place

--- Components/IntroSectionPage2/index.js
+++ Components/IntroSectionPage2/index.js
@@ -1,10 +1,8 @@
 import { Grid, Text, Card } from '@nextui-org/react';
-import css from './IntroSectionPage2.module.css'
+import css from './IntroSectionPage2.module.css';
+import Link from "next/link";
 
-// import YoutubeVideo from '../VideoPlayer';
-
-
-const IntroSectionPage2 = ({mainTitle, introSentence}) => {
+const IntroSectionPage2 = ({mainTitle, introSentence, introPara, introButton1, introButton2}) => {
     const MockItem = ({text}) => {
       return (
         <Card color="primary" css={{ h: '$24' }}>
@@ -15,22 +13,29 @@ const IntroSectionPage2 = ({mainTitle, introSentence}) => {
       );
   }
   return (
-  <Grid.Container justify="flex-start">
+  <Grid.Container gap={2} justify="center">
     <Grid xs={6}>
     <div className={css.intro}>
       <h2 className={css.mainTitle}>{mainTitle}</h2>
       <div className={css.content}>
         <h1 className={css.blackMainTitle}>{introSentence}</h1>
-        <div className={css.videoContainer}>
-      {/* <YoutubeVideo /> */}
-      </div>
+        <div className={css.introPara}><p className={css.introParaText}>{introPara}</p></div>
+
+        <div className={css.buttonContainer}>
+          <Link href="CV" passHref>
+            <button className={css.firstButton}>{introButton1}</button>
+          </Link>
+          <Link href="Home" passHref>
+            <button className={css.secondButton}>{introButton2}</button>
+          </Link>
+        </div>
       </div>
     </div>
     </Grid>
+      
   </Grid.Container>
-  
-
   ) }
 
   export default IntroSectionPage2;
+
 
--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ import IntroSectionPage2 from "../Components/IntroSectionPage2";
 import NavbarPage2 from "../Components/NavbarPage2";
 import SkillsSection from "../Components/SkillsSection";
 import ImagePage2 from "../Components/ImagePage2";
-import IntroParaPage2 from "../Components/IntroParaPage2";
+
 
 
 
@@ -16,20 +16,20 @@ export default function About() {
     return (
       <div>
       <NavbarPage2 />
-          <IntroSectionPage2 introSentence={"About me"}/>
-          {/* <AboutMeSection /> */}
-          <ImagePage2 />
-          <IntroParaPage2 
-        introPara="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          <IntroSectionPage2 introSentence={"About me"} 
+          introPara="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim 
                       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
                       nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
                       deserunt mollit anim id est laborum."
-        // mainTitle="Fullstack Software Engineer"
+        
         
         introButton1="CV"
         introButton2="Home"/>
+          {/* <AboutMeSection /> */}
+          <ImagePage2 />
+          
           {/* <SkillsHeader title="Skills"/> */}
           {/* <SkillsSection /> */}
           <Footer />
